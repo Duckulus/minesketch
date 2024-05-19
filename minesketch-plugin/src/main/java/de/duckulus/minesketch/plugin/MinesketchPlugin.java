2e8b2e6c40aaf0b17e1fca305e4d5246fa8401b7
aminh <amin.haddou@outlook.de>
2024-05-19 10:54:45 +0200

    fix compile

--- minesketch-plugin/src/main/java/de/duckulus/minesketch/plugin/MinesketchPlugin.java
+++ minesketch-plugin/src/main/java/de/duckulus/minesketch/plugin/MinesketchPlugin.java
@@ -1,14 +1,10 @@
 package de.duckulus.minesketch.plugin;
 
-import de.duckulus.minesketch.interpreter.Interpreter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinesketchPlugin extends JavaPlugin {
 
   @Override
   public void onEnable() {
-    Interpreter interpreter = new Interpreter();
-
-    getLogger().info(interpreter.interpret());
   }
 }
